832a04cf24367be3e9c245116e29297ae95f063a
VolletMaxence <mvollet@la-providence.net>
2021-09-21 15:47:43 +0200

    Correction path Icone Shellos <3

--- Monster-Hunter/Monster Hunter World/index.php
+++ Monster-Hunter/Monster Hunter World/index.php
@@ -1,7 +1,7 @@
 <html>
     <head>
         <!--Icone : -->
-        <link rel="icon" href="Icone/Shellos-Icone.ico">
+        <link rel="icon" href="../../Icone/Shellos-Icone.ico">
         <!-- CSS Bootstrap -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <!-- JS Bootstrap -->
