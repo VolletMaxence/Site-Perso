a6e18c43fc1cb78217939737ea652bb81f79626f
VolletMaxence <mvollet@la-providence.net>
2021-09-28 13:15:10 +0200

    Image Vallee

--- Sky/Esprit/Esprit.js
+++ Sky/Esprit/Esprit.js
@@ -199,101 +199,175 @@ function afficheEmotePrairie()
 
 function afficheEmoteForet()
 {
-        //Affiche des images des émotes :
-        var myImage = new Image(191, 191);
-        myImage.src = 'Image/Prairie/Base/ShiveringTrailblazer-1.png';
-        var div = document.getElementById("a");
-        div.appendChild(myImage);
+    //Affiche des images des émotes :
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Foret/Base/ShiveringTrailblazer-1.png';
+    var div = document.getElementById("a");
+    div.appendChild(myImage);
     
-        var myImage = new Image(191, 191);
-        myImage.src = 'Image/Prairie/Base/BlushingProspector-1.png';
-        var div = document.getElementById("b");
-        div.appendChild(myImage);
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Foret/Base/BlushingProspector-1.png';
+    var div = document.getElementById("b");
+    div.appendChild(myImage);
         
-        var myImage = new Image(191, 191);
-        myImage.src = 'Image/Prairie/Base/HideNSeekPioneer-1.png';
-        var div = document.getElementById("c");
-        div.appendChild(myImage);
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Foret/Base/HideNSeekPioneer-1.png';
+    var div = document.getElementById("c");
+    div.appendChild(myImage);
     
-        var myImage = new Image(191, 191);
-        myImage.src = 'Image/Prairie/Base/PoutyPorter-1.png';
-        var div = document.getElementById("d");
-        div.appendChild(myImage);
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Foret/Base/PoutyPorter-1.png';
+    var div = document.getElementById("d");
+    div.appendChild(myImage);
     
-        var myImage = new Image(191, 191);
-        myImage.src = 'Image/Prairie/Base/DismayedHunter-1.png';
-        var div = document.getElementById("e");
-        div.appendChild(myImage);
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Foret/Base/DismayedHunter-1.png';
+    var div = document.getElementById("e");
+    div.appendChild(myImage);
         
-        var myImage = new Image(191, 191);
-        myImage.src = 'Image/Prairie/Base/ApologeticLumberjack-1.png';
-        var div = document.getElementById("f");
-        div.appendChild(myImage);
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Foret/Base/ApologeticLumberjack-1.png';
+    var div = document.getElementById("f");
+    div.appendChild(myImage);
     
-        var myImage = new Image(191, 191);
-        myImage.src = 'Image/Prairie/Base/TearfulLightMiner-1.png';
-        var div = document.getElementById("g");
-        div.appendChild(myImage);
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Foret/Base/TearfulLightMiner-1.png';
+    var div = document.getElementById("g");
+    div.appendChild(myImage);
     
-        var myImage = new Image(191, 191);
-        myImage.src = 'Image/Prairie/Base/WhaleWhisperer-1.png';
-        var div = document.getElementById("h");
-        div.appendChild(myImage);
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Foret/Base/WhaleWhisperer-1.png';
+    var div = document.getElementById("h");
+    div.appendChild(myImage);
         
-        var myImage = new Image(191, 191);
-        myImage.src = 'Image/Prairie/Saison/Mimi-4117_02_provoking_performer_emote';
-        var div = document.getElementById("i");
-        div.appendChild(myImage);
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Foret/Saison/Mimi-4117_02_provoking_performer_emote';
+    var div = document.getElementById("i");
+    div.appendChild(myImage);
     
-        var myImage = new Image(191, 191);
-        myImage.src = 'Image/Prairie/Saison/Mimi-4117_03_laidback_pioneer_emote.png';
-        var div = document.getElementById("j");
-        div.appendChild(myImage);
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Foret/Saison/Mimi-4117_03_laidback_pioneer_emote.png';
+    var div = document.getElementById("j");
+    div.appendChild(myImage);
         
-        var myImage = new Image(191, 191);
-        myImage.src = 'Image/Prairie/Saison/Hair_tousle.png';
-        var div = document.getElementById("k");
-        div.appendChild(myImage);
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Foret/Saison/Hair_tousle.png';
+    var div = document.getElementById("k");
+    div.appendChild(myImage);
     
-        var myImage = new Image(191, 191);
-        myImage.src = 'Image/Prairie/Saison/Emote-blow_kisses.png';
-        var div = document.getElementById("l");
-        div.appendChild(myImage);
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Foret/Saison/Emote-blow_kisses.png';
+    var div = document.getElementById("l");
+    div.appendChild(myImage);
     
-        var myImage = new Image(191, 191);
-        myImage.src = 'Image/Prairie/Saison/Assembly-Facepalm-emote.png';
-        var div = document.getElementById("m");
-        div.appendChild(myImage);
-        
-        var myImage = new Image(191, 191);
-        myImage.src = 'Image/Prairie/Saison/Assembly-scolding-emote.png';
-        var div = document.getElementById("n");
-        div.appendChild(myImage);
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Foret/Saison/Assembly-Facepalm-emote.png';
+    var div = document.getElementById("m");
+    div.appendChild(myImage);
     
-        var myImage = new Image(191, 191);
-        myImage.src = 'Image/Prairie/Saison/Assembly-Daydream-emote.png';
-        var div = document.getElementById("o");
-        div.appendChild(myImage);
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Foret/Saison/Assembly-scolding-emote.png';
+    var div = document.getElementById("n");
+    div.appendChild(myImage);
+
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Foret/Saison/Assembly-Daydream-emote.png';
+    var div = document.getElementById("o");
+    div.appendChild(myImage);
+
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Foret/Saison/Assembly-Scaredy-emote.png';
+    var div = document.getElementById("p");
+    div.appendChild(myImage);
     
-        var myImage = new Image(191, 191);
-        myImage.src = 'Image/Prairie/Saison/Assembly-Scaredy-emote.png';
-        var div = document.getElementById("p");
-        div.appendChild(myImage);
-        
-        var myImage = new Image(191, 191);
-        myImage.src = 'Image/Prairie/Saison/Assembly-march-emote.png';
-        var div = document.getElementById("q");
-        div.appendChild(myImage);
-    
-        var myImage = new Image(191, 191);
-        myImage.src = 'Image/Prairie/Saison/Assembly-chuckling-emote.png';
-        var div = document.getElementById("r");
-        div.appendChild(myImage);
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Foret/Saison/Assembly-march-emote.png';
+    var div = document.getElementById("q");
+    div.appendChild(myImage);
+
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Foret/Saison/Assembly-chuckling-emote.png';
+    var div = document.getElementById("r");
+    div.appendChild(myImage);
 }
 
 function afficheEmoteVallee()
 {
+    //Affiche des images des émotes :
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Vallee/Base/ConfidentSightseer-1.png';
+    var div = document.getElementById("a");
+    div.appendChild(myImage);
+    
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Vallee/Base/HandstandingThrillseeker-1.png';
+    var div = document.getElementById("b");
+    div.appendChild(myImage);
+        
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Vallee/Base/MantaWhisperer-1.png';
+    var div = document.getElementById("c");
+    div.appendChild(myImage);
+    
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Vallee/Base/BackflippingChampion-1.png';
+    var div = document.getElementById("d");
+    div.appendChild(myImage);
+    
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Vallee/Base/CheerfulSpectator-1.png';
+    var div = document.getElementById("e");
+    div.appendChild(myImage);
+        
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Vallee/Base/BowingMedalist-1.png';
+    var div = document.getElementById("f");
+    div.appendChild(myImage);
+    
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Vallee/Base/ProudVictor-1.png';
+    var div = document.getElementById("g");
+    div.appendChild(myImage);
+    
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Vallee/Saison/Mimi-4117_02_leaping_dancer_emote.png';
+    var div = document.getElementById("h");
+    div.appendChild(myImage);
+        
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Vallee/Saison/Mimi-4117_03_twirling_champion_emote';
+    var div = document.getElementById("i");
+    div.appendChild(myImage);
+    
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Vallee/Saison/Belonging_sparkler.png';
+    var div = document.getElementById("j");
+    div.appendChild(myImage);
+        
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Vallee/Saison/Emote-juggle.png';
+    var div = document.getElementById("k");
+    div.appendChild(myImage);
+    
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Vallee/Saison/Season_of_Dreams_-_Spinning_mentor.png';
+    var div = document.getElementById("l");
+    div.appendChild(myImage);
+    
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Vallee/Saison/Season_of_Dreams_-_Dancing_performer.png';
+    var div = document.getElementById("m");
+    div.appendChild(myImage);
+    
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Vallee/Saison/Season_of_Dreams_-_Peeking_postman.png';
+    var div = document.getElementById("n");
+    div.appendChild(myImage);
 
+    var myImage = new Image(191, 191);
+    myImage.src = 'Image/Vallee/Saison/Season_of_Dreams_-_Bearhug_hermit.png';
+    var div = document.getElementById("o");
+    div.appendChild(myImage);
 }
 
 function afficheEmoteDesert()
